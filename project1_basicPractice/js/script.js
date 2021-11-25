0663884efc95145b6c9c933c01798e40d9f944c6
Vasili Kavalenka <clvasya@yandex.ru>
2021-11-25 17:33:06 +0300

    basicPractice

--- project1_basicPractice/js/script.js
+++ project1_basicPractice/js/script.js
@@ -1,43 +1,78 @@
 'use strict'
 
-// console.log(1);
+/* console.log(1);
 
-// console.log(number); 
+console.log(number); 
 
-// let number = 5;
+let number = 5;
 
-// const leftBorderWidth = 1;
+const leftBorderWidth = 1;
 
-// console.log(name);
+console.log(name);
 
-// var name = 'Ivan';
-// let name2 = 'Ivan'
+var name = 'Ivan';
+let name2 = 'Ivan'
 
-// {
-//     var name = 'Boris'
-//     let name2 = 'Boris'
-// }
+{
+    var name = 'Boris'
+    let name2 = 'Boris'
+}
 
-// console.log(name);
-// console.log(name2);
+console.log(name);
+console.log(name2);
 
 
-// let m;
-// console.log(typeof(m));
+let m;
+console.log(typeof(m));
 
-// let m = null;
-// console.log(typeof(m));
+let m = null;
+console.log(typeof(m));
 
-// let array = {
-//     name: 'bob'
-// }
+let array = {
+    name: 'bob'
+}
 
-// let arr = ['bob', 'beb', 'bab'];
-// console.log(arr['1']);
-// console.log(array.name);
-// console.log(typeof(arr));
+let arr = ['bob', 'beb', 'bab'];
+console.log(arr['1']);
+console.log(array.name);
+console.log(typeof(arr));
 
-// let a = '10';
+let a = '10';
 
-// console.log(typeof(+a));
+console.log(typeof(+a)); */
+
+// const num = 55
+
+// (num > 50) ? console.log('>') : (num < 50) ? console.log('<') : console.log('=')
+
+/* switch (num) {
+    case 49 : 
+        console.log ('нет');
+        break;
+    case 51 : 
+        console.log ('нет');
+        break;
+    case 55 : 
+        console.log ('да');
+        break;
+    default :
+        console.log ('неееет');
+        break;
+} */
+
+/* let a = '';
+
+switch(a) {
+    case 1:
+        console.log(typeof(a))
+        break;
+    case '':
+        console.log(typeof(a))
+        break;
+    default:
+        console.log(typeof(a))
+        break;
+} */
+    
+
 
