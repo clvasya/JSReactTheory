5d37aac174515605e8aee89e21401dfb70b039c9
Vasili Kavalenka <clvasya@yandex.ru>
2021-11-25 15:14:19 +0300

    firstCommit

---
+++ project1/js/script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// console.log(1);
+
+// console.log(number); 
+
+// let number = 5;
+
+// const leftBorderWidth = 1;
+
+// console.log(name);
+
+// var name = 'Ivan';
+// let name2 = 'Ivan'
+
+// {
+//     var name = 'Boris'
+//     let name2 = 'Boris'
+// }
+
+// console.log(name);
+// console.log(name2);
+
+
+// let m;
+// console.log(typeof(m));
+
+// let m = null;
+// console.log(typeof(m));
+
+// let array = {
+//     name: 'bob'
+// }
+
+// let arr = ['bob', 'beb', 'bab'];
+// console.log(arr['1']);
+// console.log(array.name);
+// console.log(typeof(arr));
+
+// let a = '10';
+
+// console.log(typeof(+a));
+
